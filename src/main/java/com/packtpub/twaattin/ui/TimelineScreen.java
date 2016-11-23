215b9fe64b85a7919b2079adf6760a28902446ff
Marcelo Martins <marcelomartins@santos.sp.gov.br>
2016-11-23 10:23:12 -0200

    Adjusted layout and content

--- src/main/java/com/packtpub/twaattin/ui/TimelineScreen.java
+++ src/main/java/com/packtpub/twaattin/ui/TimelineScreen.java
@@ -2,6 +2,7 @@ package com.packtpub.twaattin.ui;
 
 import static com.vaadin.server.Sizeable.Unit.PERCENTAGE;
 import static com.vaadin.ui.Alignment.BOTTOM_RIGHT;
+import static com.vaadin.ui.Alignment.MIDDLE_RIGHT;
 
 import com.packtpub.twaattin.presenter.LogoutBehavior;
 import com.vaadin.server.VaadinSession;
@@ -24,6 +25,9 @@ public class TimelineScreen extends VerticalLayout {
 
         HorizontalLayout menuBar = new HorizontalLayout(label, button);
 
+        menuBar.setWidth(100, PERCENTAGE);
+        menuBar.setComponentAlignment(button, MIDDLE_RIGHT);
+
         addComponent(menuBar);
         fillTweets();
     }
@@ -32,7 +36,11 @@ public class TimelineScreen extends VerticalLayout {
         for (int i = 0; i < 10; i++) {
             Label label = new Label();
 
-            label.setValue("Lorem ipsum dolor sit amet...");
+            label.setValue("Lorem ipsum dolor sit amet, consectetur "
+                    + "adipisicing elit, sed do eiusmod tempor incididunt "
+                    + "ut labore et dolore magna aliqua. Ut enim ad minim "
+                    + "veniam, quis nostrud exercitation ullamco laboris "
+                    + "nisi ut aliquip ex ea commodo consequat.");
             addComponent(label);
         }
     }
